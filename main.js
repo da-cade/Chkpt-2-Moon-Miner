32c5c247cb48ec31e117f9f82b2ef61b21044b83
Dylan Cade <dacade.code@gmail.com>
2022-04-01 15:11:48 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+
+//TODO Requirements
+//static data set
+  //automatic functions will have their own intervals and multipliers.
+//break reference with the data set (??)
+// Users have at least 4 upgrades to purchase
+// Each Upgrade has different modifier values and prices
+// Users can see the quantity of each upgrade they have purchased
+//click upgrade type
+//automatic upgrade type
+
+// Users can click on an image to collect a resource
+function clicker(){
+
+}
+
+// The current resource total is always displayed
+
+function total(){
+
+}
+// Users can purchase at least 1 click upgrade
+// Users can purchase at least 1 automatic upgrade
+function upgadeTotalWriter(){
+
+}
+// Automatic upgrades are applied at least every 3 seconds
+function upgradeClicker(){
+
+}
+// Users cannot purchase an upgrade if they do not have enouugh resource
+function buttonDisabler(){
+
+}
+// Purchasing an upgrade decreases the current resource total
+
+// When a user purchases an upgrade, 
+
+// NOTE Stretch goals
+
+
+
+//NOTE extras --
+// You can use marquee classes to make things move on the page.
+// bootstrap offcanvas to generate a pop up menu
+// sweet alert insufficient funds?
+
+
+
+function
